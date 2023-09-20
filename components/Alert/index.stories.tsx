31bccf40e48ea31af9270fcaac729c7bd70963f2
Calm Andy <thought7878@qq.com>
2023-09-20 09:52:04 +0800

    feat(Alert): init

---
+++ components/Alert/index.stories.tsx
@@ -0,0 +1,151 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+
+import Alert from "./";
+import React from "react";
+
+const alertMeta: ComponentMeta<typeof Alert> = {
+	title: "Components/Alert",
+	component: Alert,
+};
+export default alertMeta;
+
+//
+const Template: ComponentStory<typeof Alert> = (args) => {
+	return (
+		<div className="m-10 w-80">
+			<Alert {...args}></Alert>
+		</div>
+	);
+};
+
+//
+export const Default = Template.bind({});
+Default.args = {
+	children: "default alert",
+};
+Default.storyName = "default";
+
+//
+export const AlertWithType: ComponentStory<typeof Alert> = (args) => {
+	return (
+		<div className="m-10 w-80">
+			<div className="mb-5">
+				<Alert type="primary" {...args}>
+					primary alert
+				</Alert>
+			</div>
+			<div className="mb-5">
+				<Alert type="success" {...args}>
+					success alert
+				</Alert>
+			</div>
+			<div className="mb-5">
+				<Alert type="danger" {...args}>
+					danger alert
+				</Alert>
+			</div>
+			<div className="mb-5">
+				<Alert type="warning" {...args}>
+					warning alert
+				</Alert>
+			</div>
+		</div>
+	);
+};
+AlertWithType.storyName = "type";
+
+//
+export const AlertWithPosition: ComponentStory<typeof Alert> = (args) => {
+	return (
+		<>
+			<h2 className="text-xl text-red-500">
+				The components are displayed in the viewport position
+			</h2>
+			<div className="m-10 w-80">
+				<Alert position="left-top" {...args}>
+					left-top of viewport
+				</Alert>
+				<Alert position="left-bottom" {...args}>
+					left-bottom of viewport
+				</Alert>
+				<Alert position="right-top" {...args}>
+					right-top of viewport
+				</Alert>
+				<Alert position="right-bottom" {...args}>
+					right-bottom of viewport
+				</Alert>
+			</div>
+		</>
+	);
+};
+AlertWithPosition.storyName = "position";
+
+//
+export const AlertWithCloseBtn: ComponentStory<typeof Alert> = (args) => {
+	return (
+		<div className="m-10 w-80">
+			<div className="mb-5">
+				<Alert {...args}>default has close button</Alert>
+			</div>
+			<div className="mb-5">
+				<Alert closeBtn={false} {...args}>
+					no close button
+				</Alert>
+			</div>
+		</div>
+	);
+};
+AlertWithCloseBtn.storyName = "closeBtn";
+
+//
+export const AlertWithAutoClose: ComponentStory<typeof Alert> = (args) => {
+	return (
+		<div className="m-10 w-80">
+			<div className="mb-5">
+				<Alert autoClose {...args}>
+					3000 ms
+				</Alert>
+			</div>
+			<div className="mb-5">
+				<Alert autoClose duration={5000} {...args}>
+					5000 ms
+				</Alert>
+			</div>
+			<div className="mb-5">
+				<Alert autoClose duration={8000} {...args}>
+					8000 ms
+				</Alert>
+			</div>
+		</div>
+	);
+};
+AlertWithAutoClose.storyName = "autoClose";
+
+//
+export const AlertWithCustomClassName: ComponentStory<typeof Alert> = (
+	args
+) => {
+	return (
+		<div className="m-10 w-[600px]">
+			<Alert className="bg-purple-500 py-6 px-8" {...args}>
+				custom backgroundColor & padding with className
+			</Alert>
+		</div>
+	);
+};
+AlertWithCustomClassName.storyName = "custom style with className";
+
+//
+export const AlertWithCustomStyle: ComponentStory<typeof Alert> = (args) => {
+	return (
+		<div className="m-10 w-[600px]">
+			<Alert
+				style={{ backgroundColor: "#a855f7", padding: "1.5rem 2rem" }}
+				{...args}
+			>
+				custom backgroundColor & padding with style
+			</Alert>
+		</div>
+	);
+};
+AlertWithCustomStyle.storyName = "custom style with style";
---
+++ components/Alert/index.tsx
@@ -0,0 +1,103 @@
+import { FC, ReactNode, useEffect, useState } from "react";
+
+import { CSSTransition } from "react-transition-group";
+import { Icon } from "../Icon";
+import { IoClose } from "react-icons/io5";
+import classNames from "classnames";
+// import "./index.css";
+
+export interface AlertProps extends React.HTMLAttributes<HTMLElement> {
+	/**4 types */
+	type?: "primary" | "success" | "warning" | "danger";
+	/**Setting position */
+	position?: "right-top" | "right-bottom" | "left-top" | "left-bottom";
+	/**Whether to display the closed button */
+	closeBtn?: boolean;
+	/**Whether to automatically close */
+	autoClose?: boolean;
+	/**The duration of automatic close, the unit is millisecond */
+	duration?: number; //ms
+	/**Custom style with className */
+	className?: string; //comstomise style
+	/**Custom style with style */
+	style?: React.CSSProperties;
+	children?: ReactNode;
+}
+
+/**
+ * Alert component
+ *
+ * ```js
+ * // import like this
+ * import { Alert } from 'aui'
+ * ```
+ *
+ */
+export const Alert: FC<AlertProps> = (props) => {
+	const {
+		type,
+		children,
+		closeBtn,
+		autoClose,
+		position,
+		duration,
+		className,
+		style,
+		...rest
+	} = props;
+	const [remove, setRemove] = useState(false);
+
+	//
+	useEffect(() => {
+		if (autoClose) {
+			let timer = setTimeout(() => {
+				setRemove(true);
+			}, duration);
+
+			return () => {
+				clearTimeout(timer);
+			};
+		}
+	}, []);
+
+	// 不同情况，不同样式
+	const classes = classNames("alert", type, className, {
+		[position + ""]: position,
+	});
+
+	//
+	function handleClose() {
+		setRemove(true);
+	}
+
+	return (
+		<CSSTransition
+			in={!remove}
+			timeout={300}
+			classNames="aui-show-hide"
+			unmountOnExit
+		>
+			<div className={classes} style={style} data-testid="alert" {...rest}>
+				{children}
+				{closeBtn && (
+					<div
+						onClick={handleClose}
+						className="ml-12 inline-flex cursor-pointer"
+						data-testid="close-btn"
+					>
+						<Icon icon={<IoClose />} />
+					</div>
+				)}
+			</div>
+		</CSSTransition>
+	);
+};
+
+Alert.defaultProps = {
+	type: "primary",
+	closeBtn: true,
+	autoClose: false,
+	duration: 3000,
+};
+
+export default Alert;
