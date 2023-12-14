68a1b5bbb3f88f93c12427b0bb0836ad94138185
Calm Andy <thought7878@qq.com>
2023-12-14 15:06:45 +0800

    fix(Button): update story

--- components/Button/button.stories.tsx
+++ components/Button/button.stories.tsx
@@ -1,4 +1,3 @@
-import { ComponentMeta, ComponentStory } from "@storybook/react";
 import { Meta, StoryObj } from "@storybook/react";
 
 import { BiDownload } from "react-icons/bi";
@@ -6,251 +5,251 @@ import Button from "./button";
 import React from "react";
 
 const meta: Meta<typeof Button> = {
-	component: Button,
-	// tags: ["autodocs"],
-	title: "Components/Button",
-	tags: ["autodocs"],
-	argTypes: {},
+  component: Button,
+  // tags: ["autodocs"],
+  title: "Components/Button",
+  tags: ["autodocs"],
+  argTypes: {},
 };
 export default meta;
 
 type Story = StoryObj<typeof Button>;
 
 export const Default: Story = {
-	name: "default",
-	render: () => <Button>Default</Button>,
+  name: "default",
+  render: () => <Button>Default</Button>,
 };
 
 export const Type: Story = {
-	name: "type",
-	render: (args) => {
-		return (
-			<div className="w-[500px]">
-				<div className="mr-4 inline-block">
-					<Button btnType="primary" {...args}>
-						Press Me
-					</Button>
-				</div>
+  name: "type",
+  render: (args) => {
+    return (
+      <div className="w-[500px]">
+        <div className="mr-4 inline-block">
+          <Button btnType="primary" {...args}>
+            Press Me
+          </Button>
+        </div>
 
-				<div className="mr-4 inline-block">
-					<Button {...args}>Press Me</Button>
-				</div>
+        <div className="mr-4 inline-block">
+          <Button {...args}>Press Me</Button>
+        </div>
 
-				<div className="mr-4 inline-block">
-					<Button btnType="danger" {...args}>
-						Press Me
-					</Button>
-				</div>
-			</div>
-		);
-	},
+        <div className="mr-4 inline-block">
+          <Button btnType="danger" {...args}>
+            Press Me
+          </Button>
+        </div>
+      </div>
+    );
+  },
 };
 
 //
-export const ButtonWithSize: ComponentStory<typeof Button> = (args) => {
-	return (
-		<div>
-			<div className="mb-8">
-				<div className="mr-4 inline-block">
-					<Button size="sm" btnType="primary" {...args}>
-						Press Me
-					</Button>
-				</div>
+export const ButtonWithSize: Story = (args) => {
+  return (
+    <div>
+      <div className="mb-8">
+        <div className="mr-4 inline-block">
+          <Button size="sm" btnType="primary" {...args}>
+            Press Me
+          </Button>
+        </div>
 
-				<div className="mr-4 inline-block">
-					<Button btnType="primary" {...args}>
-						Press Me
-					</Button>
-				</div>
+        <div className="mr-4 inline-block">
+          <Button btnType="primary" {...args}>
+            Press Me
+          </Button>
+        </div>
 
-				<div className="mr-4 inline-block">
-					<Button size="lg" btnType="primary" {...args}>
-						Press Me
-					</Button>
-				</div>
-			</div>
-			<div className="mb-8">
-				<div className="mr-4 inline-block">
-					<Button size="sm" {...args}>
-						Press Me
-					</Button>
-				</div>
+        <div className="mr-4 inline-block">
+          <Button size="lg" btnType="primary" {...args}>
+            Press Me
+          </Button>
+        </div>
+      </div>
+      <div className="mb-8">
+        <div className="mr-4 inline-block">
+          <Button size="sm" {...args}>
+            Press Me
+          </Button>
+        </div>
 
-				<div className="mr-4 inline-block">
-					<Button {...args}>Press Me</Button>
-				</div>
+        <div className="mr-4 inline-block">
+          <Button {...args}>Press Me</Button>
+        </div>
 
-				<div className="mr-4 inline-block">
-					<Button size="lg" {...args}>
-						Press Me
-					</Button>
-				</div>
-			</div>
-			<div className="mb-8">
-				<div className="mr-4 inline-block">
-					<Button size="sm" btnType="danger" {...args}>
-						Press Me
-					</Button>
-				</div>
+        <div className="mr-4 inline-block">
+          <Button size="lg" {...args}>
+            Press Me
+          </Button>
+        </div>
+      </div>
+      <div className="mb-8">
+        <div className="mr-4 inline-block">
+          <Button size="sm" btnType="danger" {...args}>
+            Press Me
+          </Button>
+        </div>
 
-				<div className="mr-4 inline-block">
-					<Button btnType="danger" {...args}>
-						Press Me
-					</Button>
-				</div>
+        <div className="mr-4 inline-block">
+          <Button btnType="danger" {...args}>
+            Press Me
+          </Button>
+        </div>
 
-				<div className="mr-4 inline-block">
-					<Button size="lg" btnType="danger" {...args}>
-						Press Me
-					</Button>
-				</div>
-			</div>
-		</div>
-	);
+        <div className="mr-4 inline-block">
+          <Button size="lg" btnType="danger" {...args}>
+            Press Me
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 ButtonWithSize.storyName = "size";
 
 //
-export const ButtonWithDisabled: ComponentStory<typeof Button> = (args) => {
-	return (
-		<div>
-			<div className="mr-4 inline-block">
-				<Button btnType="primary" {...args}>
-					Press Me
-				</Button>
-			</div>
-			<div className="mr-4 inline-block">
-				<Button btnType="default" {...args}>
-					Press Me
-				</Button>
-			</div>
-			<div className="mr-4 inline-block">
-				<Button btnType="danger" {...args}>
-					Press Me
-				</Button>
-			</div>
-		</div>
-	);
+export const ButtonWithDisabled: Story = (args) => {
+  return (
+    <div>
+      <div className="mr-4 inline-block">
+        <Button btnType="primary" {...args}>
+          Press Me
+        </Button>
+      </div>
+      <div className="mr-4 inline-block">
+        <Button btnType="default" {...args}>
+          Press Me
+        </Button>
+      </div>
+      <div className="mr-4 inline-block">
+        <Button btnType="danger" {...args}>
+          Press Me
+        </Button>
+      </div>
+    </div>
+  );
 };
 ButtonWithDisabled.args = { disabled: true };
 ButtonWithDisabled.storyName = "disabled";
 
 //
-export const ButtonWithIcon: ComponentStory<typeof Button> = (args) => {
-	return (
-		<div className="w-[500px]">
-			<div className="mb-4">
-				<Button btnType="primary">
-					<BiDownload size="18px" />
-				</Button>
-			</div>
+export const ButtonWithIcon: Story = (args) => {
+  return (
+    <div className="w-[500px]">
+      <div className="mb-4">
+        <Button btnType="primary">
+          <BiDownload size="18px" />
+        </Button>
+      </div>
 
-			<div className="mb-4 flex justify-between">
-				<Button leftIcon={<BiDownload size="18px" />} btnType="primary">
-					left icon
-				</Button>
-				<Button leftIcon={<BiDownload size="18px" />}>left icon</Button>
-				<Button leftIcon={<BiDownload size="18px" />} btnType="danger">
-					left icon
-				</Button>
-			</div>
+      <div className="mb-4 flex justify-between">
+        <Button leftIcon={<BiDownload size="18px" />} btnType="primary">
+          left icon
+        </Button>
+        <Button leftIcon={<BiDownload size="18px" />}>left icon</Button>
+        <Button leftIcon={<BiDownload size="18px" />} btnType="danger">
+          left icon
+        </Button>
+      </div>
 
-			<div className="mb-4 flex justify-between">
-				<Button rightIcon={<BiDownload size="18px" />} btnType="primary">
-					right icon
-				</Button>
-				<Button rightIcon={<BiDownload size="18px" />}>right icon</Button>
-				<Button rightIcon={<BiDownload size="18px" />} btnType="danger">
-					right icon
-				</Button>
-			</div>
+      <div className="mb-4 flex justify-between">
+        <Button rightIcon={<BiDownload size="18px" />} btnType="primary">
+          right icon
+        </Button>
+        <Button rightIcon={<BiDownload size="18px" />}>right icon</Button>
+        <Button rightIcon={<BiDownload size="18px" />} btnType="danger">
+          right icon
+        </Button>
+      </div>
 
-			<div className="mb-4">
-				<Button
-					leftIcon={<BiDownload size="18px" />}
-					rightIcon={<BiDownload size="18px" />}
-					btnType="primary"
-				>
-					left right icon
-				</Button>
-			</div>
-			<div className="mb-4"></div>
-		</div>
-	);
+      <div className="mb-4">
+        <Button
+          leftIcon={<BiDownload size="18px" />}
+          rightIcon={<BiDownload size="18px" />}
+          btnType="primary"
+        >
+          left right icon
+        </Button>
+      </div>
+      <div className="mb-4"></div>
+    </div>
+  );
 };
 
 ButtonWithIcon.storyName = "icon";
 
 //
-export const ButtonWithLoading: ComponentStory<typeof Button> = (args) => {
-	return (
-		<div className="w-[500px]">
-			<div className="mb-4 flex justify-between">
-				<Button btnType="primary" loading>
-					loading...
-				</Button>
-				<Button loading>loading...</Button>
-				<Button btnType="danger" loading>
-					loading...
-				</Button>
-			</div>
-		</div>
-	);
+export const ButtonWithLoading: Story = (args) => {
+  return (
+    <div className="w-[500px]">
+      <div className="mb-4 flex justify-between">
+        <Button btnType="primary" loading>
+          loading...
+        </Button>
+        <Button loading>loading...</Button>
+        <Button btnType="danger" loading>
+          loading...
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 ButtonWithLoading.storyName = "loading";
 
 //
 //
-export const ButtonWithCustomClass: ComponentStory<typeof Button> = (args) => {
-	return (
-		<div className="w-[500px]">
-			<div className="mb-4 flex justify-between">
-				<Button
-					btnType="primary"
-					className="rounded-[1.6rem] px-[28px] py-[20px]"
-				>
-					Press Me
-				</Button>
-				<Button className="rounded-[1.6rem] px-[28px] py-[20px]">
-					Press Me
-				</Button>
-				<Button
-					btnType="danger"
-					className="rounded-[1.6rem] px-[28px] py-[20px]"
-				>
-					Press Me
-				</Button>
-			</div>
-		</div>
-	);
+export const ButtonWithCustomClass: Story = (args) => {
+  return (
+    <div className="w-[500px]">
+      <div className="mb-4 flex justify-between">
+        <Button
+          btnType="primary"
+          className="rounded-[1.6rem] px-[28px] py-[20px]"
+        >
+          Press Me
+        </Button>
+        <Button className="rounded-[1.6rem] px-[28px] py-[20px]">
+          Press Me
+        </Button>
+        <Button
+          btnType="danger"
+          className="rounded-[1.6rem] px-[28px] py-[20px]"
+        >
+          Press Me
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 ButtonWithCustomClass.storyName = "custom with className";
 //
 //
-export const ButtonWithCustom: ComponentStory<typeof Button> = (args) => {
-	return (
-		<div className="w-[500px]">
-			<div className="mb-4 flex justify-between">
-				<Button
-					btnType="primary"
-					style={{ padding: "20px 28px", borderRadius: "1.6rem" }}
-				>
-					Press Me
-				</Button>
-				<Button style={{ padding: "20px 28px", borderRadius: "1.6rem" }}>
-					Press Me
-				</Button>
-				<Button
-					btnType="danger"
-					style={{ padding: "20px 28px", borderRadius: "1.6rem" }}
-				>
-					Press Me
-				</Button>
-			</div>
-		</div>
-	);
+export const ButtonWithCustom: Story = (args) => {
+  return (
+    <div className="w-[500px]">
+      <div className="mb-4 flex justify-between">
+        <Button
+          btnType="primary"
+          style={{ padding: "20px 28px", borderRadius: "1.6rem" }}
+        >
+          Press Me
+        </Button>
+        <Button style={{ padding: "20px 28px", borderRadius: "1.6rem" }}>
+          Press Me
+        </Button>
+        <Button
+          btnType="danger"
+          style={{ padding: "20px 28px", borderRadius: "1.6rem" }}
+        >
+          Press Me
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 ButtonWithCustom.storyName = "custom with style";
--- components/Button/button.tsx
+++ components/Button/button.tsx
@@ -1,28 +1,28 @@
-import "./style/button.css";
+import './style/button.css'
 
-import { Icon } from "../Icon";
-import React from "react";
-import { Spinner } from "../Spinner";
-import classNames from "classnames";
+import { Icon } from '../Icon'
+import React from 'react'
+import { Spinner } from '../Spinner'
+import classNames from 'classnames'
 
 export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLElement> {
-	className?: string;
-	/**Set type */
-	btnType?: "primary" | "default" | "danger";
-	/**Set size*/
-	size?: "lg" | "md" | "sm";
-	/**Set disabled */
-	disabled?: boolean;
-	/**Set left icon */
-	leftIcon?: React.ReactElement;
-	/**Set right icon*/
-	rightIcon?: React.ReactElement;
-	// TODO: style bug
-	/**Set loading */
-	loading?: boolean;
+  className?: string
+  /** Set type */
+  btnType?: 'primary' | 'default' | 'danger'
+  /** Set size */
+  size?: 'lg' | 'md' | 'sm'
+  /** Set disabled */
+  disabled?: boolean
+  /** Set left icon */
+  leftIcon?: React.ReactElement
+  /** Set right icon */
+  rightIcon?: React.ReactElement
+  // TODO: style bug
+  /** Set loading */
+  loading?: boolean
 
-	children: React.ReactNode;
-	// icon?: IconProp;
+  children: React.ReactNode
+  // icon?: IconProp;
 }
 
 /**
@@ -35,30 +35,30 @@ export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLElement> {
  *
  */
 export const Button: React.FC<ButtonProps> = (props) => {
-	const {
-		btnType,
-		className,
-		loading,
-		leftIcon,
-		rightIcon,
-		disabled,
-		size,
-		children,
-		...otherProps
-	} = props;
-	//btn,btn-lg,btn-primary
-	const classes = classNames(
-		"btn",
-		{
-			[`btn-${btnType}`]: btnType,
-			[`btn-${size}`]: size,
-			disabled: disabled || loading,
-		},
-		className
-	);
+  const {
+    btnType,
+    className,
+    loading,
+    leftIcon,
+    rightIcon,
+    disabled,
+    size,
+    children,
+    ...otherProps
+  } = props
+  // btn,btn-lg,btn-primary
+  const classes = classNames(
+    'btn',
+    {
+      [`btn-${btnType}`]: btnType,
+      [`btn-${size}`]: size,
+      disabled: disabled || loading
+    },
+    className
+  )
 
-	//
-	return (
+  //
+  return (
 		<div className="inline-flex">
 			<button className={classes} disabled={disabled} {...otherProps}>
 				{leftIcon && <Icon icon={leftIcon} className="mr-1" />}
@@ -67,12 +67,12 @@ export const Button: React.FC<ButtonProps> = (props) => {
 				{rightIcon && <Icon icon={rightIcon} className="ml-1" />}
 			</button>
 		</div>
-	);
-};
+  )
+}
 //
 Button.defaultProps = {
-	disabled: false,
-	btnType: "default",
-	size: "md",
-};
-export default Button;
+  disabled: false,
+  btnType: 'default',
+  size: 'md'
+}
+export default Button
