039a156db8229dbb1ee51477b453f16dadcb2dc0
Calm Andy <thought7878@qq.com>
2024-08-27 16:04:08 +0800

    feat: init tailwind css for default demo

--- apps/web/app/number-input/default/page.tsx
+++ apps/web/app/number-input/default/page.tsx
@@ -10,19 +10,21 @@ import { ChevronDownIcon, ChevronUpIcon } from 'lucide-react';
 
 export default function Page() {
   return (
-    <>
+    <div className='pt-10 flex justify-center items-center h-screen'>
       <NumberField>
-        <NumberFieldLabel className='text-blue-500'>Count: </NumberFieldLabel>
-        <NumberFieldGroup className=''>
+        <NumberFieldLabel>Count: </NumberFieldLabel>
+        <NumberFieldGroup>
           <NumberFieldIncrement>
+            {/* ChevronUpIcon from 'lucide-react' */}
             <ChevronUpIcon className='h-4 w-4' />
           </NumberFieldIncrement>
           <NumberFieldInput />
           <NumberFieldDecrement>
+            {/* ChevronDownIcon from 'lucide-react' */}
             <ChevronDownIcon className='h-4 w-4' />
           </NumberFieldDecrement>
         </NumberFieldGroup>
       </NumberField>
-    </>
+    </div>
   );
 }
